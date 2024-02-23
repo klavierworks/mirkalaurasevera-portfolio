85b7f121702c4e9e94dd5e373ece4e7d355e111b
Andrew Hill <contact@andrewthomashill.co.uk>
2024-02-23 06:38:18 +0100

    Add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://app.pagescms.org/Runeii/mirkalaurasevera-portfolio/main/',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
