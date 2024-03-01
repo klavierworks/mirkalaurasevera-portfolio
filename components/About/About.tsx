78f7ef4196c3b4e641cbb7ded6db6264df2fc7e7
Andrew Hill <contact@andrewthomashill.co.uk>
2024-03-01 19:27:01 +0100

    Fetch rich bio from CMS

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -9,12 +9,7 @@ const About = ({ className }: AboutProps) => {
   return (
     <main className={className}>
       <article className={styles.bio}>
-        <p>..is a conceptual artist working across Photography, Video, Set Design, and Creative Direction. </p>
-        <p>With nearly two decades of expertise, she serves as a senior Creative on campaigns, crafting innovative narratives and concepts that bring a fresh perspective to the advertising industry.  </p>
-        <p>She has showcased her work in exhibitions in Milan, Berlin, and Austin, Texas.</p>
-        <p>Her unique ability to bridge the gap between the art and commercial market has garnered recognition worldwide. In 2015, she graduated from the Gerrit Rietveld Academie (Sandberg Institute) with a Master in Applied Arts and simultaneously received the prestigious ADC Young Gun Award for her commercial practice. </p>
-        <p>Currently based in Amsterdam and Paris, she is represented globally by DMB.</p>
-        <p>© All images are copyrighted and owned by Mirka Laura Severa.</p>
+        <span dangerouslySetInnerHTML={{ __html: content.bio}} />
         <a className={styles.link} target="_blank" href="mailto:mls@mirkalaurasevera.com">Work Enquiries</a>
         <a className={styles.link} target="_blank" href="mailto:contact@mirkalaurasevera.com">Press Enquiries</a>
         <a className={styles.link} target="_blank" href="https://www.instagram.com/mirkalaurasevera">Instagram</a>
