3f20d4c0c129bb5e2b1ec928898bfa29fb75e120
Andrew Hill <contact@andrewthomashill.co.uk>
2024-03-01 20:11:14 +0100

    Restore old admin redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://app.pagescms.org/Runeii/mirkalaurasevera-portfolio/main/',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
