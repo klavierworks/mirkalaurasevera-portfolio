9cdba2253cc2991dd6bac738afdb4639ca2205f3
Andrew Hill <contact@andrewthomashill.co.uk>
2024-03-01 19:45:57 +0100

    Self host admin

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/admin',
-        destination: 'https://app.pagescms.org/Runeii/mirkalaurasevera-portfolio/main/',
-        permanent: false,
-      },
-    ]
-  },
 };
 
 export default nextConfig;
