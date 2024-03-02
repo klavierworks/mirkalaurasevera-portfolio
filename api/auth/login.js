7994ecbce8ebee272139ae07c190984947933ee6
Andrew Hill <contact@andrewthomashill.co.uk>
2024-03-02 17:48:44 +0100

    Further logging

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,4 +1,6 @@
 export default async function GET(request) {
+  console.log(request.url)
+  console.log(new URL(request.url))
   const redirect_uri = `${new URL(request.url).origin}/auth/callback`;
   console.log('Redirecting to GitHub for OAuth', redirect_uri);
   console.log(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=repo`)
