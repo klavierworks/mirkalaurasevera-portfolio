d04062a55bfbbc4165013208b77531dd354433a6
Andrew Hill <contact@andrewthomashill.co.uk>
2024-03-02 17:36:07 +0100

    Actually build admin with new changes...

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,5 +1,5 @@
 export default async function GET(request) {
-  const redirect_uri = `${new URL(context.request.url).origin}/auth/callback`;
+  const redirect_uri = `${new URL(request.url).origin}/auth/callback`;
 
   return Response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=repo`, 302);
 }
