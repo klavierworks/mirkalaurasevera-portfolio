48dfedc5e3719deb10fcae907201a80128854e88
Andrew Hill <contact@andrewthomashill.co.uk>
2024-07-09 10:14:37 +0200

    Add dummy data

--- pipelines/main.ts
+++ pipelines/main.ts
@@ -6,3 +6,14 @@ import { processUnprocessedProject } from './projects';
 
 safelyProcessJsonFilesAndWrite('./content/slides', './shared/carousel.json', item => processUnprocessedSlide(item as UnprocessedSlide));
 safelyProcessJsonFilesAndWrite('./content/projects', './shared/projects.json', item => processUnprocessedProject(item as UnprocessedProject));
+
+// TEST CODE, REMOVE BEFORE GO LIVE
+safelyProcessJsonFilesAndWrite('./content/slides', './shared/projects.json', item => {
+  const slide = item as UnprocessedSlide;
+  const project: UnprocessedProject = {
+    thumbnailSrc: slide.src,
+    title: slide.line1,
+    order: slide.order,
+  }
+  return processUnprocessedProject(project)
+});
