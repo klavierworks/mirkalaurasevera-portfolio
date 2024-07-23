990773bedbcc10068f43117859037ea462e3de04
Andrew Hill <contact@andrewthomashill.co.uk>
2024-07-23 11:19:42 +0200

    Add entrance animation

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -2,6 +2,7 @@ import { CSSProperties } from "react";
 import Media from "../Media/Media";
 import styles from "./Project.module.css";
 import Link from "next/link";
+import { animated, config, useSpring } from "@react-spring/web";
 
 type ProjectProps = {
   className: string;
@@ -10,22 +11,33 @@ type ProjectProps = {
 }
 
 const Project = ({className, order, project }: ProjectProps) => {
+  const spring = useSpring({
+    from: {
+      y: 1000,
+      x: 0,
+      rotate: (order % 2 ? 1 : -1) * project.randomRotation,
+    },
+    to: {
+      y: 0,
+      x: 0,
+      rotate: (order % 2 ? 1 : -1) * project.randomRotation,
+    },
+    config: config.slow,
+    delay: 750 * Math.random()
+  })
+
   return (
     <Link
       href={`/projects/${project.slug}`}
       className={`${styles.project} ${className}`}
-      style={{
-        '--order': order,
-        '--rotate': `${project.randomRotation}deg`,
-      } as CSSProperties}
     >
-      <article>
+      <animated.article style={spring}>
         <Media 
           className={styles.media}
           media={project.thumbnail}
           isActive
         />
-      </article>
+      </animated.article>
     </Link>
   )
 }
