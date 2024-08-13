b1d94c9317271005ecea640ec056fa7c6e1c2711
Andrew Hill <contact@andrewthomashill.co.uk>
2024-08-13 18:53:42 +0200

    Restore secret gating

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+
 export const handler = async (req: NextRequest, res: NextResponse) => {
-  // if (req.headers['Authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return res.status(401).end('Unauthorized');
-  // }
+  // @ts-expect-error ignore
+  if (req.headers['Authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
+    // @ts-expect-error ignore
+    return res.status(401).end('Unauthorized');
+  }
 
   await fetch('https://api.vercel.com/v1/integrations/deploy/prj_IIUWYKS3Bjm00vaIgvytKgscaUKS/RYOOi2sE3g', {
     method: 'POST',
